f08ebb13ab35eb627602f39f50cc2ffe47296da3
Bendiz <101096042+bendiz@users.noreply.github.com>
2023-02-14 16:47:55 +0100

    Fix syntax error

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import Search from "./Search/Search";
 function App() {
   return (
     <div className="App">
+      <body>
         <div className="container">
           <Search />
         </div>
-        <footer><a href="https://github.com/bendiz/weather-react" target="_blank" rel="noreferrer" title="Opens a link in a new window"><i className="fa-brands fa-github"></i> Bendiz</a></footer>
-     </div>
+        <footer>
+          <a
+            href="https://github.com/bendiz/weather-react"
+            target="_blank"
+            rel="noreferrer"
+            title="Opens a link in a new window"
+          >
+            <i className="fa-brands fa-github"></i> Bendiz
+          </a>
+        </footer>
+      </body>
+    </div>
   );
 }
 
