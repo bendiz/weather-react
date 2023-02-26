1289e0289288ed9c3d1f8db9e02c79cc3a55a9eb
Bendiz <benerok@icloud.com>
2023-02-26 01:25:20 +0100

    Fixed forecast updating before weather was available

--- src/WeatherInfo/Forecast.js
+++ src/WeatherInfo/Forecast.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import { React } from 'react';
 import PropTypes from 'prop-types';
 
 function Forecast({ info, forecast, daysOfWeek, metric }) {
   const list = [];
   let day;
-  const description = info.description || '';
   info.date === undefined ? (day = '') : (day = info.date.getDay() - 1);
+  let forecastElements = '';
 
   if (info.date !== undefined) {
     while (list.length < 5) {
@@ -18,26 +18,27 @@ function Forecast({ info, forecast, daysOfWeek, metric }) {
       }
     }
   }
-  console.log(list);
 
-  const forecastElements = list.map((n) => {
-    return (
-      <div className="weather-forecast-one" key={n}>
-        <p className="forecast-text">
-          {metric.includes('C')
-            ? Math.round(forecast[list[n]].temperature.day)
-            : Math.round(forecast[list[n]].temperature.day * 1.8 + 32)}{' '}
-          {metric}
-        </p>
-        <img
-          src={forecast[list[n]].condition.icon_url}
-          alt={description}
-          className="weather-forecast-icon"
-        />
-        <p className="forecast-text">{daysOfWeek[list[n]].slice(0, 3)}</p>
-      </div>
-    );
-  });
+  if (forecast[0] !== undefined) {
+    forecastElements = list.map((n) => {
+      return (
+        <div className="weather-forecast-one" key={n}>
+          <p className="forecast-text">
+            {metric.includes('C')
+              ? Math.round(forecast[list[n]].temperature.day)
+              : Math.round(forecast[list[n]].temperature.day * 1.8 + 32)}{' '}
+            {metric}
+          </p>
+          <img
+            src={forecast[list[n]].condition.icon_url}
+            alt={info.description}
+            className="weather-forecast-icon"
+          />
+          <p className="forecast-text">{daysOfWeek[list[n]].slice(0, 3)}</p>
+        </div>
+      );
+    });
+  }
 
   if (forecast[0] !== undefined) {
     return <div className="Forecast bottom-section">{forecastElements}</div>;
